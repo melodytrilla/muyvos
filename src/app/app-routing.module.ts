6846b1c82f5cde64950af801d3f125114034aa73
Melody <trillamelo@gmail.com>
2020-04-03 16:17:50 -0300

    agregado de componente nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
-import {  NosotrosComponent} from './nosotros/nosotros.component';
+import { NosotrosComponent} from './nosotros/nosotros.component';
 import { AccesoriosComponent } from './accesorios/accesorios.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent},
-  { path: 'nosotros', component: NosotrosComponent},
+  { path: 'https://muyvos.netlify.com/nosotros', component: NosotrosComponent},
   { path: 'accesorios/:tipo', component: AccesoriosComponent}
 ];
 
--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -13,3 +13,4 @@ export class NosotrosComponent implements OnInit {
   }
 
 }
+
